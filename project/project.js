8ba9d75637aca33b91be23e2f05c161e4e5f2217
lderisi <luca.derisi@hotmail.it>
2022-09-05 18:51:09 +0200

    Adding truck y-foward

--- project/project.js
+++ project/project.js
@@ -60,7 +60,7 @@ function init() {
 	var floor = MESH_MANAGER.loadFromObj('floor', './assets/plane-2m.obj');
 	floor.scalate(L, L, 0);
 	var cube = MESH_MANAGER.loadFromObj('cube1', './assets/cubo_con_assi.obj');
-	var camioncino = MESH_MANAGER.loadFromObj('camion', './assets/camion.obj');
+	var camioncino = MESH_MANAGER.loadFromObj('camion', './assets/truck_total.obj');
 	cube.limits = Limits.linear(-L+0.25, L-0.25, -L+0.25, L-0.25, 3, 3);
 	cube.setPosition(0, 0, 0.25);
 	cube.scalate(0.25, 0.25, 0.25);
