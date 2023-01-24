203af7d8e39fe8eeb9176be5abd320c03728a1e7
LucaLand <luca.landolfi3@studio.unibo.it>
2023-01-24 14:51:55 +0100

    Working on light nad shadows

--- project/src/ts/webgl/webgl-wrappers.ts
+++ project/src/ts/webgl/webgl-wrappers.ts
@@ -1,3 +1,6 @@
+import {WebGLApplication} from "./webgl-application";
+import {MeshObjectManager} from "../obj/mesh-object-manager";
+
 export interface ProgramInfo {
     program: WebGLProgram;
     attribLocations: any
@@ -9,4 +12,21 @@ export class SharedUniforms {
     u_colorLight: Array<number> = [1.0, 1.0, 1.0]
     u_view: number[] = M4.identity()
     u_projection: number[] = M4.identity()
+    u_limit: number = 20
+    u_lightWorldPosition: number[] = [10, 10, 20];
+    u_color : number[] = [0.2, 0.2, 0.2, 1]
+    u_lightdirection: number[] = [0,0,1]
+    mesh: number = 0.
+    u_bias: number = 2
+    lightWorldMatrix: number[] = M4.lookAt(
+        [10, 10, 10],
+        [0, 0, 0],
+        [0, 1, 0],
+    )
+    u_reverseLightDirection: number[] = this.lightWorldMatrix.slice(8,11)
+    u_textureMatrix: number[] = M4.multiply(
+        M4.identity(),
+        M4.inverse(this.lightWorldMatrix))
+    //u_projectedTexture: WebGLTexture = WebGLApplication.prototype.environment.getCanvas().getContext("webgl").createTexture();
+
 }
