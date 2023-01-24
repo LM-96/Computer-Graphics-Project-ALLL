1efceb0bfccd14b126b286b62343b090c00e63b8
LucaLand <luca.landolfi3@studio.unibo.it>
2023-01-24 01:36:46 +0100

    Deleted all the assets

--- project/dist/src/ts/controls/user-input.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserInput = void 0;
-class UserInput {
-}
-exports.UserInput = UserInput;
-//# sourceMappingURL=user-input.js.map
